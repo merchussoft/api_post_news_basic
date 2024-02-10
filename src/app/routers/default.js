609d626ed22b3b3fc563c131eb223579350c887f
MERCHUS-SERVER\merch <merchussoft@hotmail.com>
2024-02-10 00:04:21 -0500

    se agrega una nueva funcion que es minio para guardar las imagenes

--- src/app/routers/default.js
+++ src/app/routers/default.js
@@ -1,9 +1,10 @@
 const route = require('express').Router();
-const dfci = require('../controllers/Default-controller')
+const dfci = require('../controllers/Default-controller');
+const {upload} = require('../config/multer-config');
 
 route.get('/', dfci.listarNoticias);
 route.get('/pruebasavenews/:cod', dfci.obtenerImagen);
-route.post('/savenews', dfci.insertNews);
+route.post('/savenews', upload.single('imagen'), dfci.insertNews);
 route.get('/detailspost/:cod_post', dfci.detailPost);
 
 
