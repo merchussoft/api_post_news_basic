5a8c8635a3ff58ef06d0b7e7b9d17bbf21a9243d
MERCHUS-SERVER\merch <merchussoft@hotmail.com>
2024-02-10 10:11:51 -0500

    se hace cambio de carpeta

--- src/app/config/multer-config.js
+++ src/app/config/multer-config.js
@@ -2,4 +2,5 @@ const multer = require('multer');
 
 const storage = multer.memoryStorage();
 
-exports.upload = multer({ dest: './uploads/' });
+//exports.upload = multer({ dest: './uploads/' });
+exports.upload = multer({ storage: storage });
